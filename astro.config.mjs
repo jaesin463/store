f4b479c4e3ddab693bafcefcd537000512ef77b0
Xinxe <jaesin463@gmail.com>
2026-06-15 14:18:28 +0900

    Enable search indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jaesin463.github.io',
   base: '/store',
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
