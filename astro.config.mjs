6fb45ccbbb608f32b2df0bb6d70bf6c95907d295
Xinxe <jaesin463@gmail.com>
2026-06-15 14:15:12 +0900

    Update GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jaesin463.github.io',
-  base: '/app-blog',
+  base: '/store',
   integrations: [react(), mdx()],
 
   vite: {
